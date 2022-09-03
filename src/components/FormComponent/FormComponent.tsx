8394f37662fa2f0da4bd54e919383dfeba049d5f
Tomasz Rohda <tomaszrohda@wp.pl>
2022-09-03 22:11:15 +0200

    Add header and button

--- src/components/FormComponent/FormComponent.tsx
+++ src/components/FormComponent/FormComponent.tsx
@@ -4,11 +4,12 @@ const FormComponent: FC = () => {
   return (
     <div className="formWrapper">
       <form className="myForm">
-        <p>Napisz do nas:</p>
+        <h1>Napisz do nas:</h1>
         <input type="text" placeholder="Imię i nazwisko" />
         <input type="email" placeholder="Adres e-mail *" />
         <input type="text" placeholder="+48 Numer telefonu" />
         <input type="text" placeholder="Twoja nazwa firmy" />
+        <button type="submit">Wyślij wiadomość</button>
       </form>
     </div>
   );
