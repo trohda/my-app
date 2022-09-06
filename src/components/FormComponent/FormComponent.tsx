4c7e9ebe88be786ed6083ca005a5811e63aa919d
Tomasz Rohda <tomaszrohda@wp.pl>
2022-09-06 16:14:25 +0200

    Add validation

--- src/components/FormComponent/FormComponent.tsx
+++ src/components/FormComponent/FormComponent.tsx
@@ -10,59 +10,122 @@ interface FormRequest {
 }
 
 const FormComponent: FC = () => {
+  //inputs states
   const [nameInput, setNameInput] = useState("");
   const [emailInput, setEmailInput] = useState("");
   const [telNumberInput, setTelNumberInput] = useState("");
   const [companyNameInput, setCompanyNAmeInput] = useState("");
   const [messageInput, setMessageInput] = useState("");
 
+  //validation states
+  const [inputError, setInputError] = useState(false);
+  const [emailInputError, setEmailInputError] = useState(false);
+  const [telNumberInputError, setTelNumberInputError] = useState(false);
+
   let inputRequest: FormRequest;
 
   const sendFormHandler = (event: React.FormEvent) => {
     event.preventDefault();
-    inputRequest = {
-      name: nameInput,
-      e_mail: emailInput,
-      phone_number: telNumberInput,
-      company_name: companyNameInput,
-      message: messageInput,
-    };
-    console.log(inputRequest);
+    const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    const telNumberFormat = /^\+(?:[0-9] ?){6,14}[0-9]$/;
+
+    if (
+      nameInput.length === 0 ||
+      emailInput.length === 0 ||
+      telNumberInput.length === 0 ||
+      companyNameInput.length === 0 ||
+      messageInput.length === 0
+    ) {
+      setInputError(true);
+    } else if (!emailInput.match(mailFormat)) {
+      setEmailInputError(true);
+    } else if (!telNumberInput.match(telNumberFormat)) {
+      setTelNumberInputError(true);
+    } else {
+      inputRequest = {
+        name: nameInput,
+        e_mail: emailInput,
+        phone_number: telNumberInput,
+        company_name: companyNameInput,
+        message: messageInput,
+      };
+      setEmailInputError(false);
+      setTelNumberInputError(false);
+      console.log(inputRequest);
+    }
   };
 
   return (
     <div className="formWrapper">
+      <h1>Napisz do nas:</h1>
       <form className="myForm" onSubmit={sendFormHandler}>
-        <h1>Napisz do nas:</h1>
-        <input
-          type="text"
-          placeholder="Imię i nazwisko"
-          value={nameInput}
-          onChange={(e) => setNameInput(e.target.value)}
-        />
+        <div className="formInput">
+          <input
+            type="text"
+            placeholder="Imię i nazwisko"
+            value={nameInput}
+            onChange={(e) => setNameInput(e.target.value)}
+          />
+          {inputError && nameInput.length <= 0 ? (
+            <label>Proszę uzupełnić polę</label>
+          ) : (
+            ""
+          )}
+        </div>
+        <div className="formInput">
+          <input
+            type="text"
+            placeholder="Adres e-mail *"
+            value={emailInput}
+            onChange={(e) => setEmailInput(e.target.value)}
+          />
+          {inputError && emailInput.length <= 0 ? (
+            <label>Proszę wpisać adres e-mail</label>
+          ) : (
+            ""
+          )}
+          {emailInputError && emailInput.length > 0 ? (
+            <label>Proszę wpisać prawidłowy e-mail</label>
+          ) : (
+            ""
+          )}
+        </div>
+        <div className="formInput">
+          <input
+            type="text"
+            placeholder="+48 Numer telefonu"
+            value={telNumberInput}
+            onChange={(e) => setTelNumberInput(e.target.value)}
+          />
 
-        <input
-          type="email"
-          placeholder="Adres e-mail *"
-          value={emailInput}
-          onChange={(e) => setEmailInput(e.target.value)}
-        />
-
-        <input
-          type="text"
-          placeholder="+48 Numer telefonu"
-          value={telNumberInput}
-          onChange={(e) => setTelNumberInput(e.target.value)}
-        />
-
-        <input
-          type="text"
-          placeholder="Twoja nazwa firmy"
-          value={companyNameInput}
-          onChange={(e) => setCompanyNAmeInput(e.target.value)}
-        />
-
+          {inputError && telNumberInput.length <= 0 ? (
+            <label>Proszę wpisać numer telefonu</label>
+          ) : (
+            ""
+          )}
+          {telNumberInputError && telNumberInput.length > 0 ? (
+            <label>
+              Numer musi posiadać prefix zaczynający się od + oraz min 7 cyfr
+            </label>
+          ) : (
+            ""
+          )}
+        </div>
+        <div className="formInput">
+          <input
+            type="text"
+            placeholder="Twoja nazwa firmy"
+            value={companyNameInput}
+            onChange={(e) => setCompanyNAmeInput(e.target.value)}
+          />
+          {inputError && companyNameInput.length <= 0 ? (
+            <label>Proszę uzupełnić polę</label>
+          ) : (
+            ""
+          )}
+        </div>
         <textarea
+          maxLength={1000}
           placeholder="Twoja wiadomość *"
           value={messageInput}
           onChange={(e) => setMessageInput(e.target.value)}
